39fe5bf94288fe4a4caaf8db5485019672603510
Michael <Michael.Lindner@bush.edu>
2025-07-16 16:15:07 -0700

    adds required fields for import

--- la-import.py
+++ la-import.py
@@ -31,7 +31,9 @@ col_list = [
     "Username",
 ]
 new_rows = []
-existing_emails = set(existing_students["Student Email Address"].astype(str).str.strip())
+existing_emails = set(
+    existing_students["Student Email Address"].astype(str).str.strip()
+)
 existing_emails = {s.lower() for s in existing_emails if pd.notna(s)}
 
 existing_ids = set(existing_students["Student ID Number"].astype(str).str.strip())
@@ -39,26 +41,37 @@ existing_ids = set(existing_students["Student ID Number"].astype(str).str.strip(
 updated_students = existing_students
 
 for index, row in bb_students.iterrows():
-    student_email = str(row["Student Email Address"]).strip().lower() 
+    student_email = str(row["Student Email Address"]).strip().lower()
     student_id = str(row["Student ID Number"]).strip().lower()
 
     if student_email not in existing_emails:
-        match_index = bb_students.index[bb_students["Student Email Address"].astype(str).str.strip().str.lower() == student_email][0]
+        match_index = bb_students.index[
+            bb_students["Student Email Address"].astype(str).str.strip().str.lower()
+            == student_email
+        ][0]
         print(match_index)
-        if student_id in existing_ids: 
-           updated_students.loc[updated_students["Student ID Number"] == student_id, "Student Email Address"] = student_email
-           print("updated" + student_email)
-        else: 
+        if student_id in existing_ids:
+            updated_students.loc[
+                updated_students["Student ID Number"] == student_id,
+                "Student Email Address",
+            ] = student_email
+            print("updated" + student_email)
+        else:
             new_row = {}
             for col in col_list:
                 new_row[col] = bb_students.loc[match_index, col]
+                new_row["Password"] = "TEMP_PASSWORD"
+                new_row["School Name"] = "BUSH SCHOOL"
             new_rows.append(new_row)
 
 if new_rows:
     additions = pd.DataFrame(new_rows)
     additions = additions.reindex(columns=updated_students.columns)
     additions = pd.concat([updated_students, additions], ignore_index=True)
-    additions["Do NOT Edit - Learning Ally Key"] = pd.to_numeric(additions["Do NOT Edit - Learning Ally Key"], errors="coerce").astype("Int64")
+    additions["Do NOT Edit - Learning Ally Key"] = pd.to_numeric(
+        additions["Do NOT Edit - Learning Ally Key"], errors="coerce"
+    ).astype("Int64")
     additions["Learning Ally School Org ID"] = int(129941)
     additions.to_csv("StudentImport.csv", index=False, encoding="utf-8")
     print("wrote new file")
+
